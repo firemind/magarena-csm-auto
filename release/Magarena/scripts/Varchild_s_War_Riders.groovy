df8fd1cf4f01d7e121d58291f829e800a2bc2618
PalladiaMors <unknown>
2015-03-04 04:36:09 +0100

    Add script for Varchild's War-Riders

--- release/Magarena/scripts/Varchild_s_War_Riders.groovy
+++ release/Magarena/scripts/Varchild_s_War_Riders.groovy
@@ -1,30 +1,26 @@
 [
-    new AtYourUpkeepTrigger() {
+    new MagicAtYourUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            game.doAction(new ChangeCountersAction(
-                permanent,
-                MagicCounterType.Age,
-                1
-            ));
             return new MagicEvent(
                 permanent,
                 new MagicMayChoice("Pay cumulative upkeep?"),
                 this,
-                "PN may\$ have an opponent put a 1/1 red Survivor creature token onto the battlefield for each age counter on SN. " +
-                "If he or she doesn't, sacrifice SN."
+                "Have an opponent put a 1/1 red Survivor creature token onto the battlefield for each age counter on SN."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Age,1));
+            final int age = event.getPermanent().getCounters(MagicCounterType.Age);
             if (event.isYes()) {
-                game.doAction(new PlayTokensAction(
+                game.doAction(new MagicPlayTokensAction(
                     event.getPlayer().getOpponent(),
-                    CardDefinitions.getToken("1/1 red Survivor creature token"),
-                    event.getPermanent().getCounters(MagicCounterType.Age)
+                    TokenCardDefinitions.get("1/1 red Survivor creature token"),
+                    age
                 ));
             } else {
-                game.doAction(new SacrificeAction(event.getPermanent()));
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
             }
         }
     }
