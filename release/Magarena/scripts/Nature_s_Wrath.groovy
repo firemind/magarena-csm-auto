0e37fbd2d2d00f7bed3ec40d5e1d76cc255fc5fd
PalladiaMors <unknown>
2015-03-04 02:40:02 +0100

    Add script for Nature's Wrath

--- release/Magarena/scripts/Nature_s_Wrath.groovy
+++ release/Magarena/scripts/Nature_s_Wrath.groovy
@@ -1,31 +1,27 @@
-def SWAMP_OR_BLACK_PERMANENT_YOU_CONTROL = new MagicPermanentFilterImpl() {
-    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPermanent target) {
-        return target.isController(player) &&
-               (target.hasSubType(MagicSubType.Swamp) ||
-                target.hasColor(MagicColor.Black));
-    }
+def SWAMP_OR_BLACK_PERMANENT = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasColor(MagicColor.Black) || target.hasSubType(MagicSubType.Swamp);
+    } 
 };
 
-def A_SWAMP_OR_BLACK_PERMANENT_YOU_CONTROL = new MagicTargetChoice(
-    SWAMP_OR_BLACK_PERMANENT_YOU_CONTROL,
-    "a Swamp or a black permanent to sacrifice"
+def A_SWAMP_OR_BLACK_PERMANENT = new MagicTargetChoice(
+    SWAMP_OR_BLACK_PERMANENT,
+    "a Swamp or a black permanent"
 );
 
-def ISLAND_OR_BLUE_PERMANENT_YOU_CONTROL = new MagicPermanentFilterImpl() {
-    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPermanent target) {
-        return target.isController(player) &&
-               (target.hasSubType(MagicSubType.Island) ||
-                target.hasColor(MagicColor.Blue));
-    }
+def ISLAND_OR_BLUE_PERMANENT = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasColor(MagicColor.Black) || target.hasSubType(MagicSubType.Swamp);
+    } 
 };
 
-def AN_ISLAND_OR_BLUE_PERMANENT_YOU_CONTROL = new MagicTargetChoice(
-    ISLAND_OR_BLUE_PERMANENT_YOU_CONTROL,
-    "an Island or a blue permanent to sacrifice"
+def AN_ISLAND_OR_BLUE_PERMANENT = new MagicTargetChoice(
+    ISLAND_OR_BLUE_PERMANENT,
+    "an Island or a blue permanent"
 );
 
 [
-    new OtherEntersBattlefieldTrigger() {
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent otherPermanent) {
             return otherPermanent.hasSubType(MagicSubType.Swamp) || otherPermanent.hasColor(MagicColor.Black) ?
@@ -43,11 +39,10 @@ def AN_ISLAND_OR_BLUE_PERMANENT_YOU_CONTROL = new MagicTargetChoice(
             game.addEvent(new MagicSacrificePermanentEvent(
                 event.getSource(),
                 event.getPlayer(),
-                A_SWAMP_OR_BLACK_PERMANENT_YOU_CONTROL
-            ));
-        }
+                A_SWAMP_OR_BLACK_PERMANENT
+            ));        }
     },
-    new OtherEntersBattlefieldTrigger() {
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent otherPermanent) {
             return otherPermanent.hasSubType(MagicSubType.Island) || otherPermanent.hasColor(MagicColor.Blue) ?
@@ -65,7 +60,7 @@ def AN_ISLAND_OR_BLUE_PERMANENT_YOU_CONTROL = new MagicTargetChoice(
             game.addEvent(new MagicSacrificePermanentEvent(
                 event.getSource(),
                 event.getPlayer(),
-                AN_ISLAND_OR_BLUE_PERMANENT_YOU_CONTROL
+                AN_ISLAND_OR_BLUE_PERMANENT
             ));
         }
     }
