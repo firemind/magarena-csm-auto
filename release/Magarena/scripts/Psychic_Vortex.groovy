4f35273d3a3e90d0965bcaa3101cee0697267867
PalladiaMors <unknown>
2015-03-04 14:05:36 +0100

    Add script for Psychic Vortex

--- release/Magarena/scripts/Psychic_Vortex.groovy
+++ release/Magarena/scripts/Psychic_Vortex.groovy
@@ -1,8 +1,8 @@
 [
-    new AtYourUpkeepTrigger() {
+    new MagicAtYourUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            game.doAction(new ChangeCountersAction(
+            game.doAction(new MagicChangeCountersAction(
                 permanent,
                 MagicCounterType.Age,
                 1
@@ -12,16 +12,33 @@
                 new MagicMayChoice("Pay cumulative upkeep?"),
                 this,
                 "PN may\$ draw a card for each Age counter on SN. " +
-                "If he or she doesn't, sacrifice SN."
+                " If he or she doesn't, sacrifice SN."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
-                game.doAction(new DrawAction(event.getPlayer(),event.getPermanent().getCounters(MagicCounterType.Age)));
+                game.doAction(new MagicDrawAction(event.getPlayer(),event.getPermanent().getCounters(MagicCounterType.Age)));
             } else {
-                game.doAction(new SacrificeAction(event.getPermanent()));
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
             }
+        }
+    },
+    new MagicAtEndOfTurnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer eotPlayer) {
+            return permanent.isController(eotPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN sacrifices a land and discards his or her hand."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicSacrificePermanentEvent(event.getPermanent(), MagicTargetChoice.LAND_YOU_CONTROL));
+            game.addEvent(new MagicDiscardHandEvent(event.getPermanent()));
         }
     }
 ]
