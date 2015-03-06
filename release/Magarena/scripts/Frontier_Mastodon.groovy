10164676be8d16654bcdf1721f2bd3d379bbb921
PalladiaMors <unknown>
2015-03-06 19:08:55 +0100

    Add script for Frontier Mastodon

--- release/Magarena/scripts/Frontier_Mastodon.groovy
+++ release/Magarena/scripts/Frontier_Mastodon.groovy
@@ -1,9 +1,9 @@
 [
-    new EntersWithCounterTrigger() {
+    new MagicWhenComesIntoPlayTrigger(MagicTrigger.REPLACEMENT) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            if (permanent.getController().controlsPermanent(CREATURE_POWER_4_OR_MORE)) {
-                game.doAction(ChangeCountersAction.Enters(permanent,MagicCounterType.PlusOne,1));
+            if (permanent.getController().controlsPermanent(MagicTargetFilterFactory.CREATURE_POWER_4_OR_MORE)) {
+                game.doAction(MagicChangeCountersAction.Enters(permanent,MagicCounterType.PlusOne,1));
             }
             return MagicEvent.NONE;
         }
