ba835ac07b7092d6ecdeeecc26e64ef61685cb16
PalladiaMors <unknown>
2015-03-06 18:56:14 +0100

    Add script for Dark Deal

--- release/Magarena/scripts/Dark_Deal.groovy
+++ release/Magarena/scripts/Dark_Deal.groovy
@@ -11,13 +11,20 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            for (final MagicPlayer player : game.getAPNAP()) {
-                final MagicCardList hand = new MagicCardList(player.getHand());
-                for (final MagicCard card : hand) {
-                    game.doAction(new DiscardCardAction(player,card));
-                }
-                game.doAction(new DrawAction(player, hand.size() - 1));
-            }
+            final int hand1 = event.getPlayer().getHandSize() - 1;
+            final int hand2 = event.getPlayer().getOpponent().getHandSize() - 1;
+                game.addEvent(new MagicDiscardEvent(
+                    event.getSource(),
+                    event.getPlayer(),
+                    event.getPlayer().getHandSize()
+                ));
+                game.addEvent(new MagicDiscardEvent(
+                    event.getSource(),
+                    event.getPlayer().getOpponent(),
+                    event.getPlayer().getOpponent().getHandSize()
+                ));
+                game.doAction(new MagicDrawAction(event.getPlayer(),hand1));
+                game.doAction(new MagicDrawAction(event.getPlayer().getOpponent(),hand2));
         }
     }
 ]
