0d2e54c0e3b2d40223a3da6f781920b949c2d1a5
PalladiaMors <unknown>
2015-03-11 04:52:48 +0100

    Add script for Ghoultree

--- release/Magarena/scripts/Ghoultree.groovy
+++ release/Magarena/scripts/Ghoultree.groovy
@@ -1,23 +1,22 @@
 [
-    new MagicHandCastActivation(
+     new MagicCardActivation(
         [MagicCondition.CARD_CONDITION],
         new MagicActivationHints(MagicTiming.Main, true),
         "Cast"
     ) {
         @Override
         public void change(final MagicCardDefinition cdef) {
-            cdef.setHandAct(this);
+            cdef.setCardAct(this);
         }
 
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
-            final int n = CREATURE_CARD_FROM_GRAVEYARD.filter(source).size();
-            return [
-                new MagicPayManaCostEvent(
-                    source,
-                    source.getGameCost().reduce(n)
-                )
-            ];
+            final MagicGame game = source.getGame();
+            final int n = game.filterCards(source.getController(), MagicTargetFilterFactory.CREATURE_CARD_FROM_GRAVEYARD).size();    
+            final int cost= Math.max(0,7-n)
+            return cost==0 ?
+                [new MagicPayManaCostEvent(source,"{G}")]:
+                [new MagicPayManaCostEvent(source,"{"+cost.toString()+"}{G}")];
         }
     }
 ]
