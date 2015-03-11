ad7ae4aba6f7dcd5461c44f1054ce7ec423d92a4
PalladiaMors <unknown>
2015-03-11 04:45:55 +0100

    Add script for Blasphemous Act

--- release/Magarena/scripts/Blasphemous_Act.groovy
+++ release/Magarena/scripts/Blasphemous_Act.groovy
@@ -1,23 +1,22 @@
 [
-    new MagicHandCastActivation(
+     new MagicCardActivation(
         [MagicCondition.CARD_CONDITION],
         new MagicActivationHints(MagicTiming.Main, true),
         "Cast"
     ) {
         @Override
         public void change(final MagicCardDefinition cdef) {
-            cdef.setHandAct(this);
+            cdef.setCardAct(this);
         }
 
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
-            final int n = source.getGame().getNrOfPermanents(CREATURE);
-            return [
-                new MagicPayManaCostEvent(
-                    source,
-                    source.getGameCost().reduce(n)
-                )
-            ];
+            final MagicGame game = source.getGame();
+            final int n = game.filterPermanents(MagicTargetFilterFactory.CREATURE).size();
+            final int cost= Math.max(0,8-n)
+            return cost==0 ?
+                [new MagicPayManaCostEvent(source,"{R}")]:
+                [new MagicPayManaCostEvent(source,"{"+cost.toString()+"}{R}")];
         }
     }
 ]
