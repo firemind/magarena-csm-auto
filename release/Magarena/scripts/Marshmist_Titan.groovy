a598443de8914dc90cddd3b71c6c668ff5177c12
PalladiaMors <unknown>
2015-03-11 18:32:35 +0100

    Add script for Marshmist Titan

--- release/Magarena/scripts/Marshmist_Titan.groovy
+++ release/Magarena/scripts/Marshmist_Titan.groovy
@@ -1,23 +1,21 @@
 [
-    new MagicHandCastActivation(
+     new MagicCardActivation(
         [MagicCondition.CARD_CONDITION],
         new MagicActivationHints(MagicTiming.Main, true),
         "Cast"
     ) {
         @Override
         public void change(final MagicCardDefinition cdef) {
-            cdef.setHandAct(this);
+            cdef.setCardAct(this);
         }
 
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
             final int n = source.getController().getDevotion(MagicColor.Black);
-            return [
-                new MagicPayManaCostEvent(
-                    source,
-                    source.getCost().reduce(n)
-                )
-            ];
+            final int cost= Math.max(0,6-n)
+            return cost==0 ?
+                [new MagicPayManaCostEvent(source,"{B}")]:
+                [new MagicPayManaCostEvent(source,"{"+cost.toString()+"}{B}")];
         }
     }
 ]
