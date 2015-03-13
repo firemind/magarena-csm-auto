2894922c12761072dfad961c358edf9277c28d2d
PalladiaMors <unknown>
2015-03-13 15:17:31 +0100

    Add script for Khalni Hydra

--- release/Magarena/scripts/Khalni_Hydra.groovy
+++ release/Magarena/scripts/Khalni_Hydra.groovy
@@ -1,21 +1,21 @@
 [
-    new MagicHandCastActivation(
+     new MagicCardActivation(
         [MagicCondition.CARD_CONDITION],
         new MagicActivationHints(MagicTiming.Main, true),
         "Cast"
     ) {
         @Override
         public void change(final MagicCardDefinition cdef) {
-            cdef.setHandAct(this);
+            cdef.setCardAct(this);
         }
 
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
-            final int n = source.getController().getNrOfPermanents(GREEN_CREATURE);
+            final int n = source.getController().getNrOfPermanents(MagicTargetFilterFactory.GREEN_CREATURE);
             return [
                 new MagicPayManaCostEvent(
                     source,
-                    source.getGameCost().reduce(MagicCostManaType.Green, n)
+                    source.getCost().reduce(MagicCostManaType.Green, n)
                 )
             ];
         }
