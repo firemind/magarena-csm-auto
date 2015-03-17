32e6d59ef800feaab45921d0ed021f6bef605e59
PalladiaMors <unknown>
2015-03-17 17:27:52 +0100

    Add script for Safe Haven

--- release/Magarena/scripts/Safe_Haven.groovy
+++ release/Magarena/scripts/Safe_Haven.groovy
@@ -1,21 +1,54 @@
 [
-    new AtYourUpkeepTrigger() {
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Exile"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}"),
+                new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                this,
+                "Exile target creature you control."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicExileLinkAction(
+                    event.getPermanent(),
+                    it
+                )); 
+            });
+        }
+    },
+    new MagicAtYourUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return new MagicEvent(
+            return new MagicEvent(            
                 permanent,
                 new MagicMayChoice("Sacrifice SN?"),
                 this,
-                "PN may\$ sacrifice SN. If PN does, return each card exiled with SN to the battlefield under its owner's control"
+                "PN may\$ sacrifice SN. If you do, return each card exiled with SN to the battlefield under its owner's control"
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
-                game.doAction(new SacrificeAction(event.getPermanent()));
-                game.doAction(new ReturnLinkedExileAction(
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+                game.doAction(new MagicReturnLinkedExileAction(
                     event.getPermanent(),
-                    MagicLocationType.Battlefield,
+                    MagicLocationType.Play,
                     event.getPlayer()
                 ));
             }
