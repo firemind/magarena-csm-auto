fef2d5b993a37db37965e7ffb0ea85ecad5833a1
PalladiaMors <unknown>
2015-03-17 15:29:22 +0100

    Add script for Temporal Cascade

--- release/Magarena/scripts/Temporal_Cascade.groovy
+++ release/Magarena/scripts/Temporal_Cascade.groovy
@@ -8,7 +8,7 @@ def TEXT2 = "Each player draws seven cards."
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                payedCost.isKicked() ?
+                payedCost.isKicked() ? 
                     MagicChoice.NONE :
                     new MagicOrChoice(
                         MagicChoice.NONE,
@@ -16,37 +16,44 @@ def TEXT2 = "Each player draws seven cards."
                     ),
                 this,
                 payedCost.isKicked() ?
-                    TEXT1 + " " + TEXT2 :
-                    "Choose one\$ — (1) " + TEXT1 + " (2) " + TEXT2
+                    "Each player shuffles his or her hand and graveyard into his or her library. "+
+                    "Each player draws seven cards." :
+                    "Choose one\$ — • " + TEXT1 + " • " + TEXT2 + "\$"
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isKicked() || event.isMode(1)) {
+            if (event.isKicked()) {
                 for (final MagicPlayer player : game.getAPNAP()) {
                     final MagicCardList graveyard = new MagicCardList(player.getGraveyard());
-                    for (final MagicCard card : graveyard) {
-                        game.doAction(new ShiftCardAction(
-                            card,
-                            MagicLocationType.Graveyard,
-                            MagicLocationType.TopOfOwnersLibrary
-                        ));
-                    }
                     final MagicCardList hand = new MagicCardList(player.getHand());
-                    for (final MagicCard card : hand) {
-                        game.doAction(new ShiftCardAction(
-                            card,
-                            MagicLocationType.OwnersHand,
-                            MagicLocationType.TopOfOwnersLibrary
-                        ));
-                    }
-                    game.doAction(new ShuffleLibraryAction(player));
+                for (final MagicCard card : graveyard) {
+                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                    game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersLibrary));
+                }
+                for (final MagicCard card : hand) {
+                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersHand));
+                    game.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersHand,MagicLocationType.OwnersLibrary));
+                }
+                    game.doAction(new MagicDrawAction(player,7));
+                };
+            } else if (event.isMode(1)) {
+                for (final MagicPlayer player : game.getAPNAP()) {
+                    final MagicCardList graveyard = new MagicCardList(player.getGraveyard());
+                    final MagicCardList hand = new MagicCardList(player.getHand());
+                for (final MagicCard card : graveyard) {
+                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                    game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersLibrary));
+                }
+                for (final MagicCard card : hand) {
+                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersHand));
+                    game.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersHand,MagicLocationType.OwnersLibrary));
                 }
             }
-            if (event.isKicked() || event.isMode(2)) {
-                for (final MagicPlayer player : game.getAPNAP()) {
-                    game.doAction(new DrawAction(player,7));
-                }
+            } else if (event.isMode(2)) {
+                for (final MagicPlayer player : game.getAPNAP()) { 
+                    game.doAction(new MagicDrawAction(player,7));
+                } 
             }
         }
     }
