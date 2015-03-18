1dffefc0249e6347451cea684f5e3d33ab0ed36f
PalladiaMors <unknown>
2015-03-18 22:25:05 +0100

    Add script for Incite War

--- release/Magarena/scripts/Incite_War.groovy
+++ release/Magarena/scripts/Incite_War.groovy
@@ -4,32 +4,40 @@
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                payedCost.isKicked() ?
-                    NEG_TARGET_PLAYER :
+                payedCost.isKicked() ? 
+                    MagicTargetChoice.NEG_TARGET_PLAYER :
                     new MagicOrChoice(
-                        NEG_TARGET_PLAYER,
+                        MagicTargetChoice.NEG_TARGET_PLAYER,
                         MagicChoice.NONE
                     ),
                 this,
                 payedCost.isKicked() ?
-                    "Creatures target player\$ controls attack this turn if able. "+
+                    "Creatures target player\$ controls attack this turn if able. "+ 
                     "Creatures you control gain first strike until end of turn." :
-                    "Choose one\$ — (1) Creatures target player\$ controls attack this turn if able; "+
-                    "(2) Creatures you control gain first strike until end of turn."
+                    "Choose one\$ — • Creatures target player\$ controls attack this turn if able. "+
+                    "• Creatures you control gain first strike until end of turn."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isKicked() || event.isMode(1)) {
+            if (event.isMode(1) || event.isKicked()) {
                 event.processTargetPlayer(game, {
-                    CREATURE_YOU_CONTROL.filter(it) each {
-                        game.doAction(new GainAbilityAction(it,MagicAbility.AttacksEachTurnIfAble));
+                    final Collection<MagicPermanent> targets = game.filterPermanents(
+                        it,
+                        MagicTargetFilterFactory.CREATURE_YOU_CONTROL
+                    );
+                    for (final MagicPermanent creature : targets) {
+                        game.doAction(new MagicGainAbilityAction(creature,MagicAbility.AttacksEachTurnIfAble));
                     }
                 });
             }
-            if (event.isKicked() || event.isMode(2)) {
-                CREATURE_YOU_CONTROL.filter(event) each {
-                    game.doAction(new GainAbilityAction(it,MagicAbility.FirstStrike));
+            if (event.isMode(2) || event.isKicked()) {
+                final Collection<MagicPermanent> targets2 = game.filterPermanents(
+                    event.getPlayer(),
+                    MagicTargetFilterFactory.CREATURE_YOU_CONTROL
+                );
+                for (final MagicPermanent creature2 : targets2) {
+                    game.doAction(new MagicGainAbilityAction(creature2,MagicAbility.FirstStrike));
                 }
             }
         }
