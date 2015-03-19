6272abc1571ea48bd211ed4aa7a9de6be21e3024
PalladiaMors <unknown>
2015-03-19 14:52:31 +0100

    Add script for Garruk, the Veil-Cursed

--- release/Magarena/scripts/Garruk__the_Veil_Cursed.groovy
+++ release/Magarena/scripts/Garruk__the_Veil_Cursed.groovy
@@ -1,24 +1,59 @@
 [
-    new MagicPlaneswalkerActivation(-1) {
+    new MagicPlaneswalkerActivation(1) {
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 source,
                 this,
-                "Sacrifice a creature. If PN does, PN searches his or her library for a creature card, reveals it, puts it into his or her hand, then shuffles his or her library."
+                "Put a 1/1 black Wolf creature token with deathtouch onto the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicEvent sac = new MagicSacrificePermanentEvent(event.getSource(),event.getPlayer(),SACRIFICE_CREATURE);
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("1/1 black Wolf creature token with deathtouch")));
+        }
+    },
+    new MagicPlaneswalkerActivation(-1) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+             return new MagicEvent(
+                source,
+                this,
+                "Sacrifice a creature. If you do, search your library for a creature card, reveal it, put it into your hand, then shuffle your library."
+            );
+       }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicEvent sac = new MagicSacrificePermanentEvent(event.getSource(),event.getPlayer(),MagicTargetChoice.SACRIFICE_CREATURE);
             if (sac.isSatisfied()) {
-                game.addEvent(sac);
+                final List<MagicCard> choiceList = event.getPlayer().filterCards(MagicTargetFilterFactory.CREATURE_CARD_FROM_LIBRARY);
                 game.addEvent(new MagicSearchToLocationEvent(
                     event,
-                    A_CREATURE_CARD_FROM_LIBRARY,
+                    new MagicFromCardListChoice(choiceList, 1, true),
                     MagicLocationType.OwnersHand
-                ));
-            }
+                ));           }    
+       }
+    },
+    new MagicPlaneswalkerActivation(-3) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Creatures you control gain trample and get +X/+X until end of turn, where X is the number of creature cards in your graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int X = game.filterCards(event.getPlayer(), MagicTargetFilterFactory.CREATURE_CARD_FROM_GRAVEYARD).size();  
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                event.getPlayer(),
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL
+            );
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicGainAbilityAction(target, MagicAbility.Trample));
+                game.doAction(new MagicChangeTurnPTAction(target, X, X));
+            }      
         }
     }
 ]
