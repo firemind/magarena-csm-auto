b7ebf3e430ba5b5f0fb1489dbe7a459d67fd4041
PalladiaMors <unknown>
2015-03-22 13:36:05 +0100

    Add script for Moonlit Strider

---
+++ release/Magarena/scripts/Moonlit_Strider.groovy
@@ -0,0 +1,41 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    game.doAction(new MagicGainAbilityAction(
+        event.getRefPermanent(),
+        event.getChosenColor().getProtectionAbility()
+    ));
+}
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Protection"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                this,
+                "Target creature you control\$ gains protection from the color of your choice until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.addEvent(new MagicEvent(
+                    event.getSource(),
+                    event.getPlayer(),
+                    MagicColorChoice.ALL_INSTANCE,
+                    it,
+                    action,
+                    "Chosen color\$."
+                ));
+            });
+        }
+    }
+]
