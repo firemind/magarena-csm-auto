45646c6221a74dbdf786f3b3ee64c6e9e356a91b
PalladiaMors <unknown>
2015-03-23 14:39:31 +0100

    Add script for Book Burning

--- release/Magarena/scripts/Book_Burning.groovy
+++ release/Magarena/scripts/Book_Burning.groovy
@@ -1,9 +1,9 @@
 def action = {
     final MagicGame game, final MagicEvent event ->
     if (event.isYes()) {
-        game.doAction(new DealDamageAction(event.getSource(),event.getPlayer(),6));
+        game.doAction(new MagicDealDamageAction(event.getSource(),event.getPlayer(),6));
     } else {
-        game.doAction(new MillLibraryAction(event.getRefPlayer(),6));
+        game.doAction(new MagicMillLibraryAction(event.getRefPlayer(),6));
     }
 }
 
@@ -13,9 +13,9 @@ def action = {
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                TARGET_PLAYER,
+                MagicTargetChoice.TARGET_PLAYER,
                 this,
-                "Any player may have SN deal 6 damage to him or her. If no one does, " +
+                "Unless opponent has SN deal 6 damage to him or her, " +
                 "target player\$ puts the top six cards of his or her library into his or her graveyard."
             );
         }
@@ -28,7 +28,7 @@ def action = {
                     new MagicMayChoice("have SN deal 6 damage to you?"),
                     it,
                     action,
-                    "PN may\$ have SN deal 6 damage to him or her. If PN doesn't, " +
+                    "PN may\$ have SN deal 6 damage to you. If you don't, " +
                     "RN puts the top six cards of his or her library into his or her graveyard."
                 ));
             });
