1c064f1bf8a3d164cc1247677cb94f91ee246b90
PalladiaMors <unknown>
2015-03-23 18:31:29 +0100

    Add script for Bottled Cloister

--- release/Magarena/scripts/Bottled_Cloister.groovy
+++ release/Magarena/scripts/Bottled_Cloister.groovy
@@ -1,12 +1,12 @@
 [
-    new AtUpkeepTrigger() {
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
             return permanent.isOpponent(upkeepPlayer) ?
                 new MagicEvent(
                     permanent,
                     this,
-                    "Exile all cards from PN's hand."
+                    "Exile all cards from your hand."
                 ):
                 MagicEvent.NONE;
         }
@@ -14,23 +14,30 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicCardList hand = new MagicCardList(event.getPlayer().getHand());
             for (final MagicCard card : hand) {
-                game.doAction(new ExileLinkAction(event.getPermanent(), card, MagicLocationType.OwnersHand));
+                game.doAction(new MagicExileLinkAction(
+                    event.getPermanent(),
+                    card,
+                    MagicLocationType.OwnersHand
+                ));
             }
         }
     },
-    new AtYourUpkeepTrigger() {
+    new MagicAtYourUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return new MagicEvent(
                 permanent,
                 this,
-                "Return all cards PN owns exiled with SN to his or her hand, then draw a card."
+                "Return all cards you own exiled with SN to PN's hand, then draw a card."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new ReturnLinkedExileAction(event.getPermanent(), MagicLocationType.OwnersHand));
-            game.doAction(new DrawAction(event.getPlayer(), 1));
+            game.doAction(new MagicReturnLinkedExileAction(
+                event.getPermanent(),
+                MagicLocationType.OwnersHand
+            ));
+            game.doAction(new MagicDrawAction(event.getPlayer(),1));
         }
     }
 ]
