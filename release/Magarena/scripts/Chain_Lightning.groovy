931ce6bd1d3ac64cb19cc66b4bd9c7c65ff962ba
PalladiaMors <unknown>
2015-03-26 17:39:23 +0100

    Add script for Chain Lightning

--- release/Magarena/scripts/Chain_Lightning.groovy
+++ release/Magarena/scripts/Chain_Lightning.groovy
@@ -1,7 +1,7 @@
 def action = {
     final MagicGame game, final MagicEvent event ->
     if (event.isYes()) {
-        game.doAction(new CopyCardOnStackAction(event.getPlayer(),event.getCardOnStack()));
+        game.doAction(new MagicCopyCardOnStackAction(event.getPlayer(),event.getRefCardOnStack()));
     }
 }
 
@@ -11,7 +11,7 @@ def action = {
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                NEG_TARGET_CREATURE_OR_PLAYER,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                 this,
                 "SN deals 3 damage to target creature or player\$."
             );
@@ -19,17 +19,18 @@ def action = {
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTarget(game, {
-                game.doAction(new DealDamageAction(event.getSource(),it,3));
                 game.addEvent(new MagicEvent(
                     event.getSource(),
-                    it.getController(),
+                    event.getPlayer().getOpponent(),
                     new MagicMayChoice(
                         "Pay {R}{R}?",
                         new MagicPayManaCostChoice(MagicManaCost.create("{R}{R}"))
                     ),
+                    event.getCardOnStack(),
                     action,
-                    "PN may\$ pay {R}{R}\$. If that player does, he or she may copy this spell and may choose a new target for that copy."
+                    "PN may\$ pay {R}{R}\$. If you do, copy this spell and may choose a new target for that copy."
                 ));
+                game.doAction(new MagicDealDamageAction(event.getSource(),it,3));
             });
         }
     }
