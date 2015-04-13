c90e323aeb0a87bce89b60aa30f4ac198e064dd5
PalladiaMors <unknown>
2015-04-13 21:08:08 +0200

    Add script for Phage the Untouchable

--- release/Magarena/scripts/Phage_the_Untouchable.groovy
+++ release/Magarena/scripts/Phage_the_Untouchable.groovy
@@ -1,8 +1,8 @@
 [
-    new EntersBattlefieldTrigger() {
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            return permanent.hasState(MagicPermanentState.CastFromHand) == false ?
+            return permanent.hasState(MagicPermanentState.CastFromHand) == false ? 
                 new MagicEvent(
                     permanent,
                     this,
@@ -12,7 +12,28 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new LoseGameAction(event.getPlayer()));
+            game.doAction(new MagicLoseGameAction(event.getPlayer()));
+        }
+    },
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final MagicSource source = damage.getSource();
+            return (source == permanent &&
+                    damage.isCombat() &&
+                    damage.isTargetPlayer()) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    damage.getTarget(),
+                    this,
+                    "RN loses the game."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicLoseGameAction(event.getRefPlayer()));
         }
     }
 ]
