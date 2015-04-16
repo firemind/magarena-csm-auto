2b2cd73a8ae3d265d441d24a3530e2991eea3040
PalladiaMors <unknown>
2015-04-16 03:25:32 +0200

    Add script for Balance

--- release/Magarena/scripts/Balance.groovy
+++ release/Magarena/scripts/Balance.groovy
@@ -5,25 +5,23 @@
             return new MagicEvent(
                 cardOnStack,
                 this,
-                "Except for the player with the fewest lands, each player sacrifices lands until all players control the same number of lands. " +
-                "Players discard cards and sacrifice creatures the same way."
+                "Each player chooses a number of lands he or she controls equal to the number of lands controlled by the player " +
+                "who controls the fewest, then sacrifices the rest. Players discard cards and sacrifice creatures the same way."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             for (final MagicPlayer player : game.getAPNAP()) {
                 final int n1 = player.getNrOfPermanents(MagicType.Land) - player.getOpponent().getNrOfPermanents(MagicType.Land);
-                n1.times {
+                final int n2 = player.getHandSize() - player.getOpponent().getHandSize();
+                final int n3 = player.getNrOfPermanents(MagicType.Creature) - player.getOpponent().getNrOfPermanents(MagicType.Creature);
+                for (int i=n1;i>0;i--) {
                     game.addEvent(new MagicSacrificePermanentEvent(event.getSource(),player,SACRIFICE_LAND));
                 }
-
-                final int n2 = player.getHandSize() - player.getOpponent().getHandSize();
                 if (n2 > 0) {
                     game.addEvent(new MagicDiscardEvent(event.getSource(),player,n2));
                 }
-
-                final int n3 = player.getNrOfPermanents(MagicType.Creature) - player.getOpponent().getNrOfPermanents(MagicType.Creature);
-                n3.times {
+                for (int i=n3;i>0;i--) {
                     game.addEvent(new MagicSacrificePermanentEvent(event.getSource(),player,SACRIFICE_CREATURE));
                 }
             }
