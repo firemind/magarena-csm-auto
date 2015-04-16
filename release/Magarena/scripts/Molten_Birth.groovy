4fcf4b73b4cbe09d5d56bd1f7603b4a8fffa187c
PalladiaMors <unknown>
2015-04-16 16:01:52 +0200

    Add script for Molten Birth

--- release/Magarena/scripts/Molten_Birth.groovy
+++ release/Magarena/scripts/Molten_Birth.groovy
@@ -1,46 +1,30 @@
-def winAct = {
-    final MagicGame game, final MagicEvent event ->
-    game.doAction(new ChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand))
-    game.doAction(new MoveCardAction(event.getCardOnStack()));
-}
-
-def loseAct = {
-    final MagicGame game, final MagicEvent event ->
-    final MagicLocationType oldLocation = MagicLocationType.values()[event.getRefInt()];
-    game.doAction(new ChangeCardDestinationAction(event.getCardOnStack(), oldLocation))
-    game.doAction(new MoveCardAction(event.getCardOnStack()));
-}
-
 [
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
+                new MagicCoinFlipChoice(),
                 this,
                 "PN puts two 1/1 red Elemental creature tokens onto the battlefield. "+
-                "Then PN flips a coin. If PN wins the flip, return SN to its owner's hand."
+                "Then PN flips a coin.\$ If PN wins the flip, return SN to its owner's hand."
             );
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new PlayTokensAction(
-                event.getPlayer(),
-                CardDefinitions.getToken("1/1 red Elemental creature token"),
+                event.getPlayer(), 
+                TokenCardDefinitions.get("1/1 red Elemental creature token"),
                 2
             ));
-            final MagicCardOnStack spell = event.getCardOnStack();
-            final MagicLocationType oldLocation = spell.getMoveLocation();
-
-            //prevent auto move to moveLocation after executeEvent
-            spell.setMoveLocation(MagicLocationType.Battlefield);
-
+            final Boolean heads = event.isMode(1) 
             game.addEvent(new MagicCoinFlipEvent(
-                event,
-                oldLocation.ordinal(),
-                winAct,
-                loseAct
+                event.getSource(),
+                heads,
+                event.getPlayer(),
+                new ChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand),
+                null
             ));
         }
     }
