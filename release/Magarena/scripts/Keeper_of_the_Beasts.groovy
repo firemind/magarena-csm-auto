8f62497249a5739754d842ce7b2553ed07acc25f
PalladiaMors <unknown>
2015-04-24 00:15:56 +0200

    Add script for Keeper of the Beasts

--- release/Magarena/scripts/Keeper_of_the_Beasts.groovy
+++ release/Magarena/scripts/Keeper_of_the_Beasts.groovy
@@ -1,16 +1,14 @@
 def OPPONENT_WHO_CONTROLS_MORE_CREATURES = new MagicPlayerFilterImpl() {
     public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPlayer target) {
         return target.isEnemy(player) &&
-               target.getNrOfPermanents(MagicType.Creature) > player.getNrOfPermanents(MagicType.Creature);
-    }
+               target.getNrOfPermanents(MagicType.Creature) >= player.getNrOfPermanents(MagicType.Creature);
+    } 
 };
 
 def TARGET_OPPONENT_WHO_CONTROLS_MORE_CREATURES = new MagicTargetChoice(
     OPPONENT_WHO_CONTROLS_MORE_CREATURES,
-    "target opponent who controls more creatures than you"
+    "target opponent who controlls more creatures than you"
 );
-
-def effect = MagicRuleEventAction.create("Put a 2/2 green Beast creature token onto the battlefield.");
 
 [
     new MagicPermanentActivation(
@@ -30,8 +28,15 @@ def effect = MagicRuleEventAction.create("Put a 2/2 green Beast creature token o
             return new MagicEvent(
                 source,
                 TARGET_OPPONENT_WHO_CONTROLS_MORE_CREATURES,
-                effect.getAction(),
+                this,
                 "PN puts a 2/2 green Beast creature token onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(MagicRuleEventAction.create(
+                "Put a 2/2 green Beast creature token onto the battlefield.").getEvent(event.getSource())
             );
         }
     }
