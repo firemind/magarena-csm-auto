58e87905a3e13ac2e4ba9b5708d26bf3f31fbc1f
PalladiaMors <unknown>
2015-04-24 01:12:13 +0200

    Add script for Keeper of the Mind

--- release/Magarena/scripts/Keeper_of_the_Mind.groovy
+++ release/Magarena/scripts/Keeper_of_the_Mind.groovy
@@ -1,15 +1,13 @@
 def OPPONENT_WITH_TWO_MORE_CARDS = new MagicPlayerFilterImpl() {
     public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPlayer target) {
         return target.isEnemy(player) && target.getHandSize() >= player.getHandSize() + 2;
-    }
+    } 
 };
 
 def TARGET_OPPONENT_WITH_TWO_MORE_CARDS = new MagicTargetChoice(
     OPPONENT_WITH_TWO_MORE_CARDS,
     "target opponent who has at least two more cards in hand than you"
 );
-
-def effect = MagicRuleEventAction.create("Draw a card.");
 
 [
     new MagicPermanentActivation(
@@ -29,9 +27,14 @@ def effect = MagicRuleEventAction.create("Draw a card.");
             return new MagicEvent(
                 source,
                 TARGET_OPPONENT_WITH_TWO_MORE_CARDS,
-                effect.getAction(),
+                this,
                 "Draw a card."
             );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(MagicRuleEventAction.create("Draw a card.").getEvent(event.getSource()));
         }
     }
 ]
