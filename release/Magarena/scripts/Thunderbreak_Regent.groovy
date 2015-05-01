5c86cb1e82382386b6cf44deadbcac6cc5a0779d
PalladiaMors <unknown>
2015-05-02 01:18:03 +0200

    Add script for Thunderbreak Regent

--- release/Magarena/scripts/Thunderbreak_Regent.groovy
+++ release/Magarena/scripts/Thunderbreak_Regent.groovy
@@ -1,26 +1,25 @@
-def TARGET = Permanent("Dragon you control");
-def SPELL = ItemOnStack("spell or ability an opponent controls");
+MagicTargetFilter<MagicPermanent> DRAGON_YOU_CONTROL = Permanent("Dragon you control");
 
 [
-    new BecomesTargetTrigger() {
+    new MagicWhenTargetedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicItemOnStack itemOnStack) {
             final MagicTarget target = itemOnStack.getTarget();
             return target.isPermanent() &&
-                   TARGET.accept(permanent, permanent.getController(), (MagicPermanent)target) &&
-                   SPELL.accept(permanent, permanent.getController(), itemOnStack) ?
+                   DRAGON_YOU_CONTROL.accept(permanent, permanent.getController(), (MagicPermanent)target) &&
+                   itemOnStack.getController() == permanent.getOpponent() ?
                 new MagicEvent(
                     permanent,
                     itemOnStack.getController(),
                     this,
-                    "SN deals 3 damage to PN."
+                    "SN deals 3 damage to RN."
                 ):
                 MagicEvent.NONE;
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new DealDamageAction(event.getPermanent(),event.getPlayer(),3));
+            game.doAction(new DealDamageAction(event.getPermanent(),event.getRefPlayer(),3));
         }
     }
 ]
