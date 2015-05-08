652ce81d89afd0d8d983601a1d5179022b96abe6
PalladiaMors <unknown>
2015-05-08 21:45:26 +0200

    Add script for Cruel Ultimatum

--- release/Magarena/scripts/Cruel_Ultimatum.groovy
+++ release/Magarena/scripts/Cruel_Ultimatum.groovy
@@ -1,5 +1,3 @@
-def effect = MagicRuleEventAction.create("return a creature card from your graveyard to your hand.")
-
 [
     new MagicSpellCardEvent() {
         @Override
@@ -9,20 +7,29 @@ def effect = MagicRuleEventAction.create("return a creature card from your grave
                 TARGET_OPPONENT,
                 this,
                 "Target opponent\$ sacrifices a creature, discards three cards, then loses 5 life." +
-                "PN returns a creature card from his or her graveyard to his or her hand, draws three cards, then gain 5 life."
+                "You return a creature card from your graveyard to your hand, draw three cards, then gain 5 life."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game, {
-                game.addEvent(new MagicSacrificePermanentEvent(event.getSource(), it, SACRIFICE_CREATURE));
+                game.addEvent(new MagicSacrificePermanentEvent(
+                    event.getSource(),
+                    it,
+                    SACRIFICE_CREATURE
+                ));
                 game.addEvent(new MagicDiscardEvent(event.getSource(), it, 3));
                 game.doAction(new ChangeLifeAction(it,-5));
-
-                game.addEvent(effect.getEvent(event));
-                game.doAction(new DrawAction(event.getPlayer(), 3));
-                game.doAction(new ChangeLifeAction(event.getPlayer(), 5));
             });
+            game.addEvent(new MagicEvent(
+                event.getSource(),
+                TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                MagicGraveyardTargetPicker.ReturnToHand,
+                this,
+                "Return a creature card from your graveyard to your hand."
+            ));
+            game.doAction(new DrawAction(event.getPlayer(),3));
+            game.doAction(new ChangeLifeAction(event.getPlayer(), 5));
         }
     }
 ]
