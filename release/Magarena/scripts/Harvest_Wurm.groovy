94189a81e56321139c3cf91e372907be745108b7
PalladiaMors <unknown>
2015-05-09 17:10:26 +0200

    Add script for Harvest Wurm

--- release/Magarena/scripts/Harvest_Wurm.groovy
+++ release/Magarena/scripts/Harvest_Wurm.groovy
@@ -1,22 +1,21 @@
 def effect = MagicRuleEventAction.create("return a basic land card from your graveyard to your hand.");
 
 [
-    new EntersBattlefieldTrigger() {
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
             return new MagicEvent(
                 permanent,
-                new MagicMayChoice("Return a basic land card from your graveyard to your hand?"),
+                new MagicMayChoice("Return a basic land card to your hand?"),
                 this,
-                "PN may\$ return a basic land card from his or her graveyard to his or her hand. " +
+                "PN may\$ return a basic land card from his or her graveyard to PN's hand. " +
                 "If he or she doesn't, sacrifice SN."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicEvent costEvent = effect.getEvent(event);
-            if (event.isYes() && costEvent.isSatisfied()) {
-                game.addEvent(costEvent);
+            if (event.isYes() && BASIC_LAND_CARD_FROM_YOUR_GRAVEYARD.filter(game).size() >= 1) {
+                game.addEvent(effect.getEvent(event));
             } else {
                 game.doAction(new SacrificeAction(event.getPermanent()));
             }
