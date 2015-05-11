63d1762e07cd14012eaf29b642b46ca080a9174b
PalladiaMors <unknown>
2015-05-11 20:37:50 +0200

    Add script for Wild Slash

--- release/Magarena/scripts/Wild_Slash.groovy
+++ release/Magarena/scripts/Wild_Slash.groovy
@@ -1,3 +1,11 @@
+def cantBePrevented = new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.CANT_BE_PREVENTED) {
+    @Override
+    public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+        damage.setUnpreventable();
+        return MagicEvent.NONE;
+    }
+}
+
 [
     new MagicSpellCardEvent() {
         @Override
@@ -6,15 +14,15 @@
                 cardOnStack,
                 NEG_TARGET_CREATURE_OR_PLAYER,
                 this,
-                "If PN controls a creature with power 4 or greater, damage can't be prevented this turn. " +
-                "SN deals 2 damage to target creature or player\$."
+                "If you control a creature with power 4 or greater, damage can't be prevented this turn." +
+                " SN deals 2 damage to target creature or player\$."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTarget(game, {
                 if (event.getPlayer().controlsPermanent(CREATURE_POWER_4_OR_MORE)) {
-                    game.doAction(new AddTurnTriggerAction(IfDamageWouldBeDealtTrigger.CantBePrevented));
+                    game.doAction(new AddTurnTriggerAction(cantBePrevented));
                 }
                 game.doAction(new DealDamageAction(event.getSource(),it,2));
             });
