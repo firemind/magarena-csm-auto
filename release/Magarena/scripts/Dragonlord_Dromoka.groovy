6ac7640dedb57000145320b9401c4410bc74f0f7
PalladiaMors <unknown>
2015-05-15 03:25:35 +0200

    Add script for Dragonlord Dromoka

--- release/Magarena/scripts/Dragonlord_Dromoka.groovy
+++ release/Magarena/scripts/Dragonlord_Dromoka.groovy
@@ -2,13 +2,12 @@
     new MagicStatic(MagicLayer.Player) {
         @Override
         public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
-            if (source.isOpponent(player)) {
-                player.setState(MagicPlayerState.CantCastSpells);
-            }
+            final MagicPlayer opponent = source.getOpponent();
+            opponent.setState(MagicPlayerState.CantCastSpells);
         }
         @Override
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source.isController(game.getTurnPlayer())
+            return game.getTurnPlayer() == source.getController();
         }
     }
 ]
