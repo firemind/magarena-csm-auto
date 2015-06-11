4c2678b862c6b20a5d48b1858d6dbf5c80b76a91
a. benedict balbuena (chuckie) <unknown>
2015-06-11 06:36:10 +0200

    Add script for Greater Good

--- release/Magarena/scripts/Greater_Good.groovy
+++ release/Magarena/scripts/Greater_Good.groovy
@@ -1,13 +1,12 @@
 [
     new MagicPermanentActivation(
-        [new MagicArtificialCondition(MagicConditionFactory.HandAtLeast(3))],
         new MagicActivationHints(MagicTiming.Draw),
         "Draw"
     ) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
             return [
-                new MagicSacrificePermanentEvent(source, SACRIFICE_CREATURE)
+                new MagicSacrificePermanentEvent(source,SACRIFICE_CREATURE)
             ];
         }
 
@@ -15,19 +14,20 @@
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 source,
+                this,
                 payedCost.getTarget(),
                 this,
-                "PN draw cards equal to RN's power, then discards three cards."
+                "PN draw cards equal to the power of RN then discard three cards."
             );
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer player = event.getPlayer();
-            final int amount=event.getRefPermanent().getPower();
-            game.logAppendValue(player,amount);
-            game.doAction(new DrawAction(player, amount));
-            game.addEvent(new MagicDiscardEvent(event.getSource(), player, 3));
+            event.processTarget(game, {
+                final MagicPermanent sacrificed=event.getRefPermanent();
+                game.doAction(new DrawAction(event.getSource(),it,sacrificed.getPower()));
+        game.addEvent(new MagicDiscardEvent(event.getSource(),player,3));
+            });
         }
     }
 ]
