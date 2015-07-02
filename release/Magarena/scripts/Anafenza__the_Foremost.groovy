43a0d227fb32ac4ed4dae488a53b9f52cf58fc6f
PalladiaMors <unknown>
2015-07-02 17:06:48 +0200

    Add script for Anafenza, the Foremost

--- release/Magarena/scripts/Anafenza__the_Foremost.groovy
+++ release/Magarena/scripts/Anafenza__the_Foremost.groovy
@@ -1,25 +1,39 @@
+def ANOTHER_TAPPED_CREATURE_YOU_CONTROL = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPermanent target) {
+        return target != source && target.isTapped() && target.isCreature() && target.isFriend(player);
+    } 
+};
+
+def ANOTHER_TARGET_TAPPED_CREATURE_YOU_CONTROL = new MagicTargetChoice(
+    ANOTHER_TAPPED_CREATURE_YOU_CONTROL,
+    "another target tapped creature you control"
+);
+
 [
-    new LeavesBattlefieldTrigger(MagicTrigger.REPLACEMENT) {
+    new MagicWhenSelfAttacksTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final RemoveFromPlayAction act) {
-            final MagicPermanent it = act.getPermanent();
-            if (it.isNonToken() &&
-                it.isCreature() &&
-                it.getCard().isEnemy(permanent) &&
-                act.to(MagicLocationType.Graveyard)) {
-                act.setToLocation(MagicLocationType.Exile);
-            }
-            return MagicEvent.NONE;
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent creature) {
+            return new MagicEvent(
+                permanent,
+                ANOTHER_TARGET_TAPPED_CREATURE_YOU_CONTROL,
+                this,
+                "Whenever SN attacks, put a +1/+1 counter on another target tapped creature you control\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new ChangeCountersAction(it,MagicCounterType.PlusOne,1));
+            });
         }
     },
-    new WouldBeMovedTrigger(MagicTrigger.REPLACEMENT) {
+    new MagicWouldBeMovedTrigger(MagicTrigger.REPLACEMENT) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MoveCardAction act) {
             if (act.card.hasType(MagicType.Creature) &&
-                act.card.isToken() == false &&
-                act.from(MagicLocationType.Battlefield) == false &&
                 act.card.isEnemy(permanent) &&
-                act.to(MagicLocationType.Graveyard)) {
+                act.getToLocation() == MagicLocationType.Graveyard) {
                 act.setToLocation(MagicLocationType.Exile);
             }
             return MagicEvent.NONE;
