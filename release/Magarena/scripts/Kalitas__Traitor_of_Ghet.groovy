c524afd6d1f9ebee750e0cf32a561e49a293b3ae
Guest <unknown>
2016-05-12 17:27:05 +0200

    Add script for Propaganda

---
+++ release/Magarena/scripts/Kalitas__Traitor_of_Ghet.groovy
@@ -0,0 +1,14 @@
+[
+    new WouldBeMovedTrigger(MagicTrigger.REPLACEMENT) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MoveCardAction act) {
+            if (act.card.hasType(MagicType.Creature) &&
+                act.card.isEnemy(permanent) &&
+                act.to(MagicLocationType.Graveyard) &&
+                act.from(MagicLocationType.Battlefield)) {
+                act.setToLocation(MagicLocationType.Exile);
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
