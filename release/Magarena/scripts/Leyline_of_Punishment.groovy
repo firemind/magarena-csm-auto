d5d3faacc9450c06a232bd93495e647c456430fd
Guest <system@firemind.ch>
2016-07-24 04:12:13 +0200

    Add script for Leyline of Punishment

---
+++ release/Magarena/scripts/Leyline_of_Punishment.groovy
@@ -0,0 +1,15 @@
+[
+
+    IfDamageWouldBeDealtTrigger.CantBePrevented    
+
+    new IfLifeWouldChangeTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final ChangeLifeAction act) {
+            if (act.getLifeChange() > 0) {
+                act.setLifeChange(0);
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
+
