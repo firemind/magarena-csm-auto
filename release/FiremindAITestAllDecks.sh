9a31f0321adb9a4be956c4d0722d36235044ccab
Mike <mike@otep.ch>
2018-05-28 23:25:37 +0200

    add test runner that uses all decks in directory

---
+++ release/FiremindAITestAllDecks.sh
@@ -0,0 +1,15 @@
+#java -ea -cp Magarena.jar magic.DeckStrCal \
+AI1=GMCTS
+AI2=MCTS
+AI1S=${1:-1}
+AI2S=${2:-1}
+POOL=${3-Magarena/decks/prebuilt/}
+java -cp Magarena.jar magic.AiStrCal \
+  --ai1 $AI1 \
+  --str1 $AI1S \
+  --ai2 $AI2 \
+  --str2 $AI2S \
+  --deckpool $POOL\
+  --games 2 | tee -a aitestresults/$AI1-$AI1S-$AI2-$AI2S.log
+
+#  --deck2 Magarena/decks/prebuilt/Black_and_White.dec \
